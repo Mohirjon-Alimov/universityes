40e46488f20a4c32317d940d63a10ceb2308796c
Mohirjon <mohir7209@gmail.com>
2022-04-18 21:53:29 +0500

    git init

---
+++ js/main.js
@@ -0,0 +1,51 @@
+var elBall = document.querySelector("#score");
+var elBtn = document.querySelector(".btn");
+var elOut = document.querySelector(".out");
+var elSelection = document.querySelector("#univer");
+var message = "";
+
+var universityes = ["tatu", "milliy", "tdtu", "sampi"];
+
+
+for (var university of universityes) {
+  var newOpt = document.createElement("option");
+  newOpt.value = university;
+  newOpt.textContent = university;
+  elSelection.append(newOpt);
+}
+
+
+elBtn.addEventListener("click", function(e) {
+  e.preventDefault();
+
+  var ball = elBall.value.trim();
+  var ballNumber = Number(ball);
+
+  if (ball == "" || ball.length >= 4){
+    elBall.style.borderColor = "red";
+    return
+  } else if (isNaN(ballNumber)){
+    elBall.style.borderColor = "red";
+    return
+  }
+
+
+  elBall.style.borderColor = "black";
+
+  if (elBall.value >=120) {
+    message = `grant asosida oqishga qabul qilindingiz`;
+  } else if (elBall.value >=65) {
+    message = `kontrakt shartnomasi asosida oqishga qabul qilindingiz`;
+  } else if (elBall.value >=50) {
+    message = `superkontrakt shartnomasi asosida oqishga qabul qilindingiz`;
+  } else if (elBall.value < 50) {
+    message = `qabul qilinmadingiz`
+  }
+
+  
+  var newP = document.createElement("p");
+  elOut.appendChild(newP);
+  newP.textContent = `siz ${elSelection.value}ga ${message}`;
+  
+})
+
